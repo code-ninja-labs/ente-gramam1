dc94479a976dff553a44cec5d4a9cdf884b8400c
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-02-08 21:41:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Autorickshaw from './pages/Autorickshaw'; // Import the new component
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Home Route */}
         <Route path="/" element={<Home />} />
+
+        {/* Autorickshaw Route */}
+        <Route path="/autorickshaw" element={<Autorickshaw />} />
       </Routes>
     </Router>
   );
