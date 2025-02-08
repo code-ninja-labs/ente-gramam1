3e5bb4c59822afc053964e40e234a8ba11717b84
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-02-08 22:52:10 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Use the environment variables
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://wubzoqmcskpsbdtjpseb.supabase.co";
+const supabaseAnonKey = "=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1YnpvcW1jc2twc2JkdGpwc2ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkwMzE5MjksImV4cCI6MjA1NDYwNzkyOX0.0PuoBFw3l-cMU7xw6ssk8UBh9Fu4t9AwKBt1ps33JDc";
 
 // Create Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
