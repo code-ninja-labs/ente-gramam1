638dccf849dc7e24f1d99fcf118bda36ad83dd5f
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-02-08 18:14:55 +0530

    Create Autorickshaw.jsx

---
+++ src/pages/Autorickshaw.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Autorickshaw = () => {
+  return (
+    <div>
+      <h1>Autorickshaw Page</h1>
+      <p>Details about autorickshaw services.</p>
+    </div>
+  );
+};
+
+export default Autorickshaw;
