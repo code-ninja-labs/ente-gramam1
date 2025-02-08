b48c1cbe04c0a019f8e1181f7da88549b99fd811
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-02-08 18:03:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
