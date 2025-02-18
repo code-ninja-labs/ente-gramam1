bb6270c09dd6a425f491c711c7499608b06cd59e
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-02-18 23:19:48 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,10 +14,15 @@ const Home = () => {
       document.body.appendChild(script);
 
       script.onload = () => {
-        window._shareustag = window._shareustag || {cmd: []};
+        window._shareustag = window._shareustag || { cmd: [] };
+
         window._shareustag.cmd.push(function() {
           var slot1 = window._shareustag.defineSlot('/shareus-admanager/3311712454/aHrokQdEJZ/pan', [[970, 90], [728, 90], [320, 50], [300, 250], [336, 280]], 'shareus-admanager-3311712454-aHrokQdEJZ')
             .addService(window._shareustag.pubads());
+
+          var slot2 = window._shareustag.defineSlot('/shareus-admanager/3311712454/zRjipJmaSH/mobile', [[320, 50], [320, 100], [468, 60]], 'shareus-admanager-3311712454-zRjipJmaSH')
+            .addService(window._shareustag.pubads());
+
           window._shareustag.enableServices();
         });
       };
@@ -34,6 +39,7 @@ const Home = () => {
         <p>Select a service below to proceed:</p>
         <AppIcons /> {/* App-Style Icons */}
         <div id="shareus-admanager-3311712454-aHrokQdEJZ"></div> {/* Ad Container */}
+        <div id="shareus-admanager-3311712454-zRjipJmaSH"></div> {/* New Ad Container */}
       </main>
       <footer className="footer">
         <p>© {new Date().getFullYear()} Ente Gramam. All rights reserved.</p> {/* Dynamic Year */}
