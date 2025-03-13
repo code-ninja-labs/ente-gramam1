58b0d02697f3a59892855bb9912554b61238e9a8
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-03-13 15:59:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-plugins: [react()],
-base: './', // Ensures relative asset paths in the production build
+  plugins: [react()],
+  base: './', // Ensures relative asset paths in the production build
 });
