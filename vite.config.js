581844d4160d813db41d024a75d15289caa704e7
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-03-13 15:58:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import copy from 'vite-plugin-copy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    copy({
-      targets: [
-        { src: 'public/*', dest: 'cordova-app/www' },
-      ]
-    })
-  ],
-  build: {
-    outDir: 'cordova-app/www',      // <-- Output directory
-    emptyOutDir: true
-  },
-  base: './', // Ensures relative asset paths in the production build
+plugins: [react()],
+base: './', // Ensures relative asset paths in the production build
 });
