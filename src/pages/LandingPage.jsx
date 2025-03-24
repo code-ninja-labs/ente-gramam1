29ab0395728d10fad92c6528c8ad8fd44c02a4c4
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-03-24 15:43:52 +0530

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import './LandingPage.css';
 
 const LandingPage = ({ session }) => {
   const navigate = useNavigate();
