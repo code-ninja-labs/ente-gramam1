006be0a0945fce4569a4e671dddd8c6b47ec3ffa
Hike-projects <160869264+Hike-projects@users.noreply.github.com>
2025-03-24 15:42:04 +0530

    Update Autorickshaw.jsx

--- src/pages/Autorickshaw.jsx
+++ src/pages/Autorickshaw.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
-import "./Autorickshaw.css";
 
 const Autorickshaw = () => {
   const [drivers, setDrivers] = useState([]);
